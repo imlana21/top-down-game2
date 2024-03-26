9ca8f1d6ca10c132503ece3ece7cd2ba287bd070
imlana21 <ihwan.cfc@gmail.com>
2024-03-27 06:31:53 +0700

    fix door room1

--- scene/world/warp/warpworld_room1.gd
+++ scene/world/warp/warpworld_room1.gd
@@ -1,11 +1,25 @@
 extends Node2D
 
+var boss_killed: bool = false
+
 func _ready():
 	call_bos()
 
 func call_bos():
 	var boss_slime = load("res://scene/enemies/slime/slime.tscn").instantiate()
 	boss_slime.type = "bos"
-	boss_slime.modulate = "3160ff"
-	boss_slime.global_position = Vector2(1400, -50)
+	boss_slime.global_position = Vector2(50, -50)
 	$EnemyList.add_child(boss_slime)
+
+func _on_room1_to_corridor1(body):
+	# if boss_killed:
+		for door in $".."/Corridor1/DoorList.get_children():
+			door.close_the_door()
+
+func _on_room1_to_room2_player_entered(body):
+	for door in $DoorList.get_children():
+		door.open_the_door()
+
+func _on_to_room1_player_entered(body):
+	for door in $DoorList.get_children():
+		door.close_the_door()

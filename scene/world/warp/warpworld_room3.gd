6399667e4ee43ba61552fd16ad00b8f3b2796315
imlana21 <ihwan.cfc@gmail.com>
2024-03-28 07:04:05 +0700

    add door

--- scene/world/warp/warpworld_room3.gd
+++ scene/world/warp/warpworld_room3.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
+var enemies_pos: Array
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	for slime in $EnemyList.get_children():
+		slime.visible = false
+		enemies_pos.append(slime.position)		
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func call_slimes():
+	await get_tree().create_timer(1).timeout
+	for slime in $EnemyList.get_children():
+		$EnemyList.remove_child(slime)	
+	await get_tree().create_timer(2).timeout
+	for pos in enemies_pos:
+		Autoload.spawn_enemy($EnemyList, pos, "normal")
+	for slime in $EnemyList.get_children():
+		slime.visible = false
